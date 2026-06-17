6a11dd2a48b1cf5d3a00af28c0f542794323b3fc
SourceAirbender <grios019@gmail.com>
2026-06-17 18:26:00 +0100

    added license, notice, and headers to files

--- src/stockpile_panel.cpp
+++ src/stockpile_panel.cpp
@@ -1,4 +1,24 @@
-﻿#include "stockpile_panel.h"
+﻿// dfcapture - multiplayer Dwarf Fortress in the browser, as a DFHack plugin
+// Copyright (C) 2026 Gabriel Rios
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Runs on DFHack (Zlib); descends from DFPlex (Zlib) and webfort (ISC).
+// Full license: see LICENSE. Third-party credits: see NOTICE.
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+#include "stockpile_panel.h"
 
 #include "Core.h"
 #include "json_util.h"
